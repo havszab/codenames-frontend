dbe1d4a18c4a7f28b45fa17815249d97e75893ff
havszab <havaldaszab@gmail.com>
2019-02-07 21:54:00 +0100

    images added to game info

--- src/components/Information/GameInfo.js
+++ src/components/Information/GameInfo.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-const GameInfo = (props) => (
-    <div>
-        <div>
-                {props.title}
-        </div>
-        <div>
-            {props.body}
-        </div>
-        <img src={props.path} alt=""/>
-    </div>
-);
+class GameInfo extends React.Component  {
+
+    render() {
+        const imgPath = "../../assets/information_images/" + this.props.path;
+
+        return (
+            <div>
+                <div>
+                    {this.props.title}
+                </div>
+                <div>
+                    {this.props.body}
+                </div>
+                <img src={require("../../assets/information_images/" + this.props.path)} alt=""/>
+            </div>
+        )
+    }
+
+}
 
 export default GameInfo;
